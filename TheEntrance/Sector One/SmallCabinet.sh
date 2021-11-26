b57b0e9ea44cbcc8ca2a7211aaa4fb19289aa001
NoahBurton <94695867+NoahBurton@users.noreply.github.com>
2021-11-25 22:45:03 -0500

    Update and rename SmallCabinate.sh to SmallCabinet.sh

---
+++ TheEntrance/Sector One/SmallCabinet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "You open the cabinet drawer and it extends and extends"
+sleep 5
+echo "The drawer finally comes to a rest and you see what appears to be old banking info now crossed out"
+sleep 1
+echo "NAME Rashawn Cormier Sr./
+ CREDIT CARD #4̶9̶1̶6̶-̶0̶1̶3̶0̶-̶2̶4̶6̶7̶-̶4̶6̶4̶8̶  /
+ BANK ACC # 5̶3̶4̶9̶9̶7̶7̶4̶8 /
+ ROUTING # 1̶2̶5̶2̶0̶0̶9̶3̶4̶  /
+ IBAN U̶S̶0̶3̶0̶0̶0̶4̶6̶6̶5̶4̶9̶4̶4̶6̶9̶7̶7̶3̶9̶4̶2̶0̶7̶3̶5̶3̶ 
