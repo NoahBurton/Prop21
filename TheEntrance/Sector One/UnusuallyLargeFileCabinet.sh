c957e8dfd3238bb6de89a8f87c4a90b1fc9b1788
NoahBurton <94695867+NoahBurton@users.noreply.github.com>
2021-11-26 14:41:45 -0500

    Update UnusuallyLargeFileCabinet.sh

--- TheEntrance/Sector One/UnusuallyLargeFileCabinet.sh
+++ TheEntrance/Sector One/UnusuallyLargeFileCabinet.sh
@@ -2,6 +2,8 @@
 
 echo "After opening the large obsidian file cabinet it extends out almost taking you out in the process. When the cabinet is at a rest you riffled through the papers you find your eyes rested upon an NFT, big money if you can haul it back to the entrance!"
 
+echo " "
+
 echo "What NFT do you see? abstract or dog?"
 
 read answ
@@ -25,6 +27,11 @@ if [ $answ = "dog" ];
  echo "   /  \ /  \ "  >> art
 fi
 
-echo " To move this beautiful art you can use the command mv art .."
-echo " The ".." means the file will be moved up one directory, very useful for this situation!"
-echo " HOW TO MOVE FILES WITH MV NORMALLY"
+echo " "
+echo " To move this beautiful art you can use the command mv art ..  "
+echo " The ".." means the file will be moved up one directory, very useful for this situation! "
+echo " Depending on where your file is you may need to move it up multiple directories which can be done by using the same command we used above but modified"
+echo " An example would be mv FileName ../../ to move the file up two directories"
+echo " "
+echo " To move a file down you can use the command this way mv FileName dir1/dir2/ these directories this is pathed from the current directory you would be in, example would be YouAreHere/dir1/dir2"
+echo " In the example given above the file would be moved to the dir2 directory"
