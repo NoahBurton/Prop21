dc523a29a7a2dba3a4537165f5d2f4318291857e
Llama-lord <94424996+Llama-lord@users.noreply.github.com>
2021-11-26 17:16:29 -0500

    Update UnusuallyLargeFileCabinet.sh

--- TheEntrance/Sector One/UnusuallyLargeFileCabinet.sh
+++ TheEntrance/Sector One/UnusuallyLargeFileCabinet.sh
@@ -32,6 +32,18 @@ echo " To move this beautiful art you can use the command mv art ..  "
 echo " The ".." means the file will be moved up one directory, very useful for this situation! "
 echo " Depending on where your file is you may need to move it up multiple directories which can be done by using the same command we used above but modified"
 echo " An example would be mv FileName ../../ to move the file up two directories"
+
+
+ 
 echo " "
 echo " To move a file down you can use the command this way mv FileName dir1/dir2/ these directories this is pathed from the current directory you would be in, example would be YouAreHere/dir1/dir2"
 echo " In the example given above the file would be moved to the dir2 directory"
+
+read art
+
+if [$art = "abstract" ];
+ then
+ mv abstract sector1/Entrance
+ else
+ mv dog sector1/Entrance
+fi
