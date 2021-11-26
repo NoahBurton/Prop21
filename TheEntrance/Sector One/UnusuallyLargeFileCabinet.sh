e884b77f0c3c48e9a467e1b9621be0a934a19771
Llama-lord <94424996+Llama-lord@users.noreply.github.com>
2021-11-26 00:24:13 -0500

    Update UnusuallyLargeFileCabinet.sh

--- TheEntrance/Sector One/UnusuallyLargeFileCabinet.sh
+++ TheEntrance/Sector One/UnusuallyLargeFileCabinet.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+echo "placehodler text"
+sleep 10
+echo "placeholder text"
+sleep 3
+echo "placeholder information 1"
+echo "placeholder information 2"
+echo "placeholder information 3"
+echo "placeholder information 4"
+
+#Not sure if you wanted to use the size of this cabinet to allow the user to utlize the grep utility or not but the option is there.
+
 #copys art to Entrance
+
+read answ
+echo "Does the cabinet contain the information you seek?"
+
+if [$answ = "yes" -o $answ = "YES" ]; 
+ then
+ echo "Fantastic, now use the cp utlitity to copy and move the art to the Entrance."
+ else 
+ echo "Nothing here.  Either look harder or look elsewhere."
+
+#ex: cp art Entrance
