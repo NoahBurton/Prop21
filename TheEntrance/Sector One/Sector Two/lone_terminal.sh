513ae28b00b9db61daefed3800bc29c6a11e378c
Llama-lord <94424996+Llama-lord@users.noreply.github.com>
2021-11-27 14:31:32 -0500

    Create lone_terminal.sh

---
+++ TheEntrance/Sector One/Sector Two/lone_terminal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ["$0" = "ls" ]; then
+ echo "Taking a quick glance around the area you see that there are a number of object of interest."
+ echo "Investigate whichever strikes your fancy."
+fi
+
+
+
+if ["$0" = "./lone_terminal" ]; 
+ then 
+ echo "Before you sits a terminal, perhaps containing a fair deal of useful information."
+ echo "A log-in is required.  Looks like the system requires a name and account number."
+ echo "Maybe you've found something in the previous sector that can be of use?"
+fi
+
+if ["$0" = 
+ 
+ 
