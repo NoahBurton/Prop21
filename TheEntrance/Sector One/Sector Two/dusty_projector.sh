43b2099406ef466c3e12b79c3f94c051dc1bd059
NoahBurton <94695867+NoahBurton@users.noreply.github.com>
2021-11-28 17:03:07 -0500

    Update dusty_projector.sh

--- TheEntrance/Sector One/Sector Two/dusty_projector.sh
+++ TheEntrance/Sector One/Sector Two/dusty_projector.sh
@@ -10,15 +10,15 @@ echo "Take a closer look? y/n"
 read answ
 
 if [ $answ = "y" ]; then
- echo "The projector flickers on and off, the image of a face filling the screen."
- echo "A small keypad showing its age emits a bright flash and blinks to life with numbers and names. Maybe a name will help find some anwsers?"
+ echo "The projector flickers on and off, the image of the man you are here to stop filling the screen."
+ echo "A small keypad showing its age emits a bright flash and blinks to life with numbers. Maybe entering the command move files will help you move the numbers in order?"
 fi
 
 read input
 
-if [ $input = "Rashawn_Cormier_Sr" ]; then
- echo "The Projector whirls as images start to spill onto the screen numbers and images start flashing quickly."
- echo "The numbers starting to slow as the machine hums to a dull click as the numbers are placed onto the screen."
+if [ $input = "mv" ]; then
+ echo "The Projector whirls as the numbers start to spill onto the screen numbers and images start flashing quickly."
+ echo "The numbers starting to slow as the machine hums to a dull click as the numbers are placed in order onto the screen."
  echo "You make out several numbers: 8-6-7-5"
  echo "They could be of some use down the line."
 fi
