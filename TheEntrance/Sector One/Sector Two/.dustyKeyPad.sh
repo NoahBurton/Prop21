f6af3cc9fd13beec4a10a34f74a98cbdac646dc4
Llama-lord <94424996+Llama-lord@users.noreply.github.com>
2021-11-30 18:04:23 -0500

    Update .dustyKeyPad.sh

--- TheEntrance/Sector One/Sector Two/.dustyKeyPad.sh
+++ TheEntrance/Sector One/Sector Two/.dustyKeyPad.sh
@@ -6,10 +6,10 @@ echo "A keypad!  It looks like it take 4-digit inputs."
 read - p "Might you have any idea what the passcode is?" digi
 if [$digi = "8675" ]; then
  echo "You hear a click come from within the table.  Curious."
- echo "Be sure to back up and check what made the noise."
+ echo "It came from right in front of you, so using ls -a is not necessary in this instance."
  
  cp .tablePanel tablePanel
- mv tablePanel Sector Two
+ 
  else
  echo "Password was incorrect"
  fi
