250a4d4e73503f4a9aa525e6006416f8b5b83e51
Llama-lord <94424996+Llama-lord@users.noreply.github.com>
2021-12-02 11:17:53 -0500

    Update .dustyKeyPad.sh

--- TheEntrance/Sector One/Sector Two/.dustyKeyPad.sh
+++ TheEntrance/Sector One/Sector Two/.dustyKeyPad.sh
@@ -2,8 +2,9 @@
 
 echo "Under a layer of dust thick enough to choke an elephant you see it."
 echo "A keypad!  It looks like it take 4-digit inputs."
+echo "Might you have any idea what the passcode is?"
 
-read - p "Might you have any idea what the passcode is?" digi
+read digi
 if [$digi = "8675" ]; then
  echo "You hear a click come from within the table.  Curious."
  echo "It came from right in front of you, so using ls -a is not necessary in this instance."
