30988f1a5467d3c0101e90f5f58cb6876aef467a
Llama-lord <94424996+Llama-lord@users.noreply.github.com>
2021-12-02 11:19:16 -0500

    Update dusty_table.sh

--- TheEntrance/Sector One/Sector Two/dusty_table.sh
+++ TheEntrance/Sector One/Sector Two/dusty_table.sh
@@ -7,7 +7,7 @@ echo "but the chairs look damn comfy.  A closer look my be in order."
 
 echo "Want to check it out?"
 
-read -p "Closer look?" answ
+read answ
 
 if [$answ = "yes" -o $answ = "YES" ]; then
  echo "Leave no stone unturned as they say.  Use the input 'ls -a' to be thorough in your search."
@@ -15,9 +15,9 @@ else
 echo "You don't find the table to be of enough interest to check out right now."
 fi
 
+echo "If you've found any clues or codes, you could try them now."
 
-
-read -p "If you've found any clues now may be the time to use them." digi
+read digi
 
 if [$digi = "8675" ]; then
  echo "Jackpot!  The code you've entered opened a secret compartment."
