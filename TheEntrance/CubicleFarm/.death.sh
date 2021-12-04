259050cf6ec14375db400e1e7fe9518bbf26426e
NoahBurton <94695867+NoahBurton@users.noreply.github.com>
2021-12-03 21:39:47 -0500

    Update .death.sh

--- TheEntrance/CubicleFarm/.death.sh
+++ TheEntrance/CubicleFarm/.death.sh
@@ -1,5 +1,124 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #!/bin/bash
-        
+
         echo
         echo "With your anwser he dives to a button on the phone and your cyberdeck goes off with warnings GOD has been notified and you need to get out of this area!"
         echo "When you try to run you can't move, GOD must be stopping you and putting your legs to sleep!"
@@ -7,18 +126,18 @@
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------------"
         echo "Using Crtl C you can exit out of processes in Linux try it now to escape!"
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------------"
-        
-ctrlC_count=0=0
+
+ctrlC_count=0
 
 function no_ctrlc()
 {
-    let ctrlC_count=0++
+    let ctrlC_count++
     echo
-    if [[ $ctrlC_count=0 == 1 ]]; then
+    if [[ $ctrlC_count == 1 ]]; then
         echo "You are under arrest please stop resisting guards are dispatched to take care of you soon."
-    elif [[ $ctrlC_count=0 == 2 ]]; then
+    elif [[ $ctrlC_count == 2 ]]; then
         echo "Please stop your life is forfit and now the companies proporty please remain still."
-     elif [[ $ctrlC_count=0 == 3 ]]; then
+     elif [[ $ctrlC_count == 3 ]]; then
         echo "This is your last chance stop resisting or you be destroyed as per trash disposal company policy"
     else
         echo "You finally break free as you type quickly freeing your digital self for a quick get away into the cubical maze to hide from your pursuers"
@@ -38,6 +157,7 @@ done
     echo "Prop21 fails to pass as Brackhaven denies the proposition"
     echo "-----------------------------------------------------------------------------------------------------"
     echo "          GAME OVER"
-    
+
     #rm -rf Prop21
 }
+
