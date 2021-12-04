8fb7d283a1185c01d9ad997446412f0fd7d85b9a
NoahBurton <94695867+NoahBurton@users.noreply.github.com>
2021-12-03 19:32:23 -0500

    Create TiredLookingMan.sh

---
+++ TheEntrance/CubicleFarm/TiredLookingMan.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "A tired looking business man stares a screen of excel sheets in his small cramped cubical"
+echo "After some typing his eyes hover over to you"
+echo "'Need anything?' he says in a deadpan voice"
+echo
+echo "He must not be who you are looking for"
+echo "You tell him no and he looks more tired than annoyed as he gets back to work"
