90aa06862fdf28df6cdbf8d6bef3e3afb8b987c9
NoahBurton <94695867+NoahBurton@users.noreply.github.com>
2021-12-03 22:33:41 -0500

    Update Larry.sh

--- TheEntrance/CubicleFarm/Accounting/Larry.sh
+++ TheEntrance/CubicleFarm/Accounting/Larry.sh
@@ -1,1 +1,95 @@
-trade crypto and talk to him he is a cryto addict
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#trade crypto and talk to him he is a cryto addict
+
+#!/bin/bash
+
+if [ 'ls | grep -c art' == 1 ]; then
+        echo "art test"
+        rm art
+        
+     elif ['ls | grep -c doge' == 1 ]; then
+        echo "doge test"
+        rm doge
+else
+  echo "fail test"
