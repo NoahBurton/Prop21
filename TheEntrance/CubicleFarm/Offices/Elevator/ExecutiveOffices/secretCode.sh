3b17bbf77537416b7ec3e63466703a7ec0514da4
Llama-lord <94424996+Llama-lord@users.noreply.github.com>
2021-12-05 10:37:17 -0500

    Update secretCode.sh

--- TheEntrance/CubicleFarm/Offices/Elevator/ExecutiveOffices/secretCode.sh
+++ TheEntrance/CubicleFarm/Offices/Elevator/ExecutiveOffices/secretCode.sh
@@ -115,9 +115,11 @@
 
 
 
-echo "The code to the holy grail is here.  Would you like to know what it is?"
+echo "The code within will provide you access to the final prize."
+echo "But the script to do so is a bit messed up."
+echo "The vim editor can be utilized to fix this script so take a crack at it."
 
 read answ
-if [answ = "yes" ] || [answ = "YES" ];then
+if [answ = "yes" ] || [$answ = "YES" ];then
 echo To reveal that which has yet to be seen, enter code open."
 fi
