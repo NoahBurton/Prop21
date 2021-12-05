94504d4a0e164fd546b8cb65fc24e1f6f4a141b0
NoahBurton <94695867+NoahBurton@users.noreply.github.com>
2021-12-04 22:52:56 -0500

    Update Larry.sh

--- TheEntrance/CubicleFarm/Accounting/Larry.sh
+++ TheEntrance/CubicleFarm/Accounting/Larry.sh
@@ -84,12 +84,34 @@
 
 #!/bin/bash
 
+echo "'Well you remember the deal? An NFT for the information and the code'"
+echo "He rubs his hands together quickly with anticipatiin"
+echo "Well do you have the NFT here or do you needa get it still?"
+
 if [ "`ls | grep -c art`" == "1" ]; then
-        echo "art test"
+        echo
+        echo "'This is amazing! Elegant and exquisite like aways'"
+        echo "He takes the NFT and stashes it and with a quick flick a thumbdrive is flung from, his sleeve to his hand"
+        echo "'This should contain all you need the code and how to break the safe, just be careful any wrong steps and game over for both of us'"
+        echo 
+        echo "The file contains the code word 8055 as well as a program to hack into the safe to disable the code to give you access"
+        echo "The program is called 'vim' you can use this to edit scripts when hovering over letters in vim you can press x to delete the letter which you will need to in this case"
+        echo "the 'i' key will allow you to insert words however for the safe this may be unnecessary, to get out of insert mode you will press ':'"
+        echo "When you are done with editing the file in vim you can type wq! to save and exit or q! to exit without saving you have to be in the ':' mode to quit however"
+        
         rm art
         
      elif [ "`ls | grep -c doge`" == 1 ]; then
-        echo "doge test"
+        echo
+        echo "'The most beautiful dog I have ever seen'"
+        echo "He takes the NFT and stashes it and with a quick flick a thumbdrive is flung from, his sleeve to his hand"
+        echo "'This should contain all you need the code and how to break the safe, just be careful any wrong steps and game over for both of us'"
+        echo 
+        echo "The file contains the code word 8055 as well as a program to hack into the safe to disable the code to give you access"
+        echo "The program is called 'vim' you can use this to edit scripts when hovering over letters in vim you can press x to delete the letter which you will need to in this case"
+        echo "the 'i' key will allow you to insert words however for the safe this may be unnecessary, to get out of insert mode you will press ':'"
+        echo "When you are done with editing the file in vim you can type wq! to save and exit or q! to exit without saving you have to be in the ':' mode to quit however"
+        
         rm doge
 else
   echo "fail test"
